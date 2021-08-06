bdb4188a71e3e83d1ed0020068cf84fd33cd031f
JOBA25 <jorge_octavio25@outlook.com>
2021-08-06 17:06:15 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const logger = require('./loggerMiddleware')
+
+app.use(cors())
+app.use(express.json())
+
+app.use(logger)
+
+let notes = [
+  {
+    id: 1,
+    content: 'Hola cara de bola',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+/*  const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(notes))
+})
+*/
+
+app.get('/', (request, response) => {
+  response.send('<h1>Holiiiii</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    console.log({ note })
+  } else {
+    response.status(404).end()
+  }
+
+  response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  if (!note || !note.content) {
+    return response.statusCode(400).jason({
+      error: 'note.content is missing'
+    })
+  }
+
+  const ids = notes.map(note => note.id)
+  const maxId = Math.max(...ids)
+
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof note.important !== 'undefined' ? note.important : false,
+    date: new Date().toISOString()
+
+  }
+  notes = [...notes, newNote]
+  response.status(201).json(newNote)
+})
+
+app.use((request, response) => {
+  console.log(request.path)
+  response.status(404).json({
+    error: 'Not found'
+  })
+})
+
+const PORT = 3001
+//  la iniciación del servidor en express es asincrono
+app.listen(PORT, () => {
+  console.log(`servidor corriendo en el puerto ${PORT}`)
+})
